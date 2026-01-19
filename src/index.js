2ab0569d2171adf5e64c5b9c37ca05e5d6b37cc3
Your Name <you@example.com>
2026-01-19 16:49:14 +0300

    hw2/1

--- src/index.js
+++ src/index.js
@@ -2,57 +2,60 @@ const http = require("http");
 const getUsers = require("./modules/users.js");
 
 const port = 3000;
-const hostname = "http://127.0.0.1";
+const hostname = "127.0.0.1";
 
 const server = http.createServer((request, response) => {
-  const ipAddress = "http://127.0.0.1";
-  const url = new URL(request.url, ipAddress);
+  const url = new URL(request.url, `http://${hostname}:${port}`);
+  const hasHelloParam = url.searchParams.has("hello");
   const userName = url.searchParams.get("hello");
 
-  if (userName) {
+  // 1. Обработка hello
+  if (hasHelloParam) {
+    if (userName && userName.trim()) {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.setHeader("Content-Type", "text/plain; charset=utf-8");
+      response.write(`Hello, my friend ${userName}`);
+      response.end();
+      return;
+    } else {
+      response.statusCode = 400;
+      response.statusMessage = "Bad Request";
+      response.setHeader("Content-Type", "text/plain; charset=utf-8");
+      response.write("Enter a name");
+      response.end();
+      return;
+    }
+  }
+
+  // 2. Обработка users
+  if (url.pathname === "/users") {
     response.statusCode = 200;
-    response.statusMessage = "ok";
-    response.setHeader("Content-Type", "text/plain");
-    response.write(`Hello, my friend ${userName}`);
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "application/json; charset=utf-8");
+    response.write(getUsers());
     response.end();
     return;
   }
 
-  switch (request.url) {
-    case "/users":
-      response.statusCode = 200;
-      response.statusMessage = "OK";
-      response.setHeader("Content-Type", "application/json");
-      response.write(getUsers());
-      response.end();
-      break;
-
-    case "/?hello":
-      response.statusCode = 400;
-      response.statusMessage = "Bad Request";
-      response.setHeader("Content-Type", "text/plain");
-      response.write(`Enter a name`);
-      response.end();
-      break;
-
-    case "/":
-      response.statusCode = 200;
-      response.statusMessage = "OK";
-      response.setHeader("Content-Type", "text/plain");
-      response.write(`Hello world`);
-      response.end();
-      break;
-
-    default:
-      response.statusCode = 500;
-      response.statusMessage = "Internal Server Error";
-      response.setHeader("Content-Type", "text/plain");
-      response.write("wrong");
-      response.end();
-      break;
+  // 3. Корень
+  if (url.pathname === "/") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "text/plain; charset=utf-8");
+    response.write("Hello world");
+    response.end();
+    return;
   }
+
+  // 4. Неизвестные запросы
+  response.statusCode = 500;
+  response.statusMessage = "Internal Server Error";
+  response.setHeader("Content-Type", "text/plain; charset=utf-8");
+  response.write("wrong");
+  response.end();
 });
 
 server.listen(port, () => {
-  console.log(`Сервер запущен по адресу ${hostname}:${port}/`);
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
 });
